9dd68e57c7887fb3dc3bc1410e867082a073ed57
sooyeon lee <2sy0127@gmail.com>
2021-11-26 15:24:43 +0900

    BOJ

--- 백준/15649.py
+++ 백준/15649.py
@@ -1,5 +1,5 @@
-def solution(target_num, tmp_num, arr, tmp_arr):
-    if target_num == tmp_num:
+def solution(target_num, arr, tmp_arr):
+    if target_num == len(tmp_arr):
         for num in tmp_arr:
             print(num, end=' ')
         return
@@ -7,7 +7,7 @@ def solution(target_num, tmp_num, arr, tmp_arr):
     for i in range(len(arr)):
         if arr[i] not in tmp_arr:
             tmp_arr.append(arr[i])
-            solution(target_num, tmp_num + 1, arr, tmp_arr)
+            solution(target_num, arr, tmp_arr)
             tmp_arr.pop()
 
 
@@ -15,4 +15,4 @@ if __name__ == "__main__":
 
     n, m = map(int, input().split())
     arr = [i for i in range(1, n + 1)]
-    solution(m, 0, arr, [])
+    solution(m, arr, [])
---
+++ 백준/15650.py
@@ -0,0 +1,12 @@
+
+def solution(target_num, tmp_num, arr, tmp_arr):
+    if target_num == tmp_num:
+        for num in tmp_arr:
+
+
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    arr = [i for i in range(1, n+1)]
+    solution(m, 0, arr, [])
