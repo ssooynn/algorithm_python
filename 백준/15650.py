16546bb08028efdd88e1d6e4ac942b53b2fa1acb
sooyeon lee <2sy0127@gmail.com>
2021-11-26 15:35:10 +0900

    BOJ

--- 백준/15650.py
+++ 백준/15650.py
@@ -1,9 +1,17 @@
 
-def solution(target_num, tmp_num, arr, tmp_arr):
-    if target_num == tmp_num:
+def solution(target_num, index, target_arr, tmp_arr):
+    if index > len(target_arr):
+        return
+    if len(tmp_arr) == target_num:
         for num in tmp_arr:
+            print(num, end=' ')
+        return
 
-
+    for i in range(index, len(target_arr)):
+        tmp_arr.append(arr[i])
+        solution(target_num, i+1, target_arr, tmp_arr)
+        tmp_arr.pop()
+    return
 
 
 if __name__ == "__main__":
